71cf88df90bcf6ff237e1fd891d61979c6fa9f97
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-02 16:23:19 -0600

    Cleaned up services.js

--- services.js
+++ services.js
@@ -1,9 +1,3 @@
-// for(var i=0; i<6; i++){
-// const serviceContainer = document.createElement('div')
-// serviceContainer.className = "service_" + i
-// document.body.appendChild(serviceContainer)
-// }
-
 for(var i=0; i<6; i++){
 document.getElementById("services").innerHTML +=
 `<div class = "service">
@@ -14,24 +8,3 @@ document.getElementById("services").innerHTML +=
 <div class = "service_details>
 </div>`
 }
-
-// for(var i=0; i<6; i++){
-//     document.body.innerHTML += `<div class="service_' + i + '"> <div class="img_container"><img class="service_img"/> <div class="overlay"> <div class="service_details"> </div></div></div></div>`
-// }
-
-// const div1 = document.createElement('div');
-// div1.className = "service_'+ i'";
-
-// const div2 = document.createElement('div');
-// div2.className = "img_container";
-
-// const image = document.createElement('img');
-// image.className = "service_img";
-
-// const div3 = document.createElement('div');
-// div3.className = "overlay";
-
-// const div4 = document.createElement('div');
-// div4.className = "service_details";
-
-// document.body.appendChild(servicesLoop);
