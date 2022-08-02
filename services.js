91210dd267cafa97aa85e45fc82031041c1513fc
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-02 11:16:04 -0600

    Updated services.js

--- services.js
+++ services.js
@@ -1,7 +1,12 @@
-
 for(var i=0; i<6; i++){
-    document.body.innerHTML += '<div class="service_' + i + '"> <div class="img_container"><img class="service_img"/> <div class="overlay"> <div class="service_details"> </div></div></div></div>'
+const servicesContainer = document.createElement('div')
+servicesContainer.id = 'services_container'
+document.body.appendChild(servicesContainer)
 }
+
+// for(var i=0; i<6; i++){
+//     document.body.innerHTML += '<div class="service_' + i + '"> <div class="img_container"><img class="service_img"/> <div class="overlay"> <div class="service_details"> </div></div></div></div>'
+// }
 
 // const div1 = document.createElement('div');
 // div1.className = "service_'+ i'";
