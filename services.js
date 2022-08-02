85ea62f39b9310659a85769b4da8f7805fd1a1ca
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-02 09:08:43 -0600

    Updated servics.js

--- services.js
+++ services.js
@@ -1,12 +1,21 @@
-for(var i =0; i<6; i++){
-    document.list_of_services.innerHTML += 
-    `<div class="service_'+ i' ">
-        <div class="img_container"> 
-            <img class="service_img">
-                <div class="overlay">
-                </div>
-        </div>
-    <div class="service_details">
-      </div>
-    </div>`
+const servicesLoop = document.createDocumentFragment();
+
+for(var i=0; i<6; i++){
+    const div1 = document.createElement('div');
+    div1.className = "service_'+ i'";
+
+    const div2 = document.createElement('div');
+    div2.className = "img_container";
+
+    const image = document.createElement('img');
+    image.className = "service_img";
+
+    const div3 = document.createElement('div');
+    div3.className = "overlay";
+
+    const div4 = document.createElement('div');
+    div4.className = "service_details";
+
+    document.appendChild(servicesLoop);
+
 }
