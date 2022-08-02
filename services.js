9a79a38768931b49143ec505e6f3ec9cc06105a2
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-02 08:47:02 -0600

    Created JS loop for containers

---
+++ services.js
@@ -0,0 +1,12 @@
+for(var i =0; i<6; i++){
+    document.list_of_services.innerHTML += 
+    `<div class="service_'+ i' ">
+        <div class="img_container"> 
+            <img class="service_img">
+                <div class="overlay">
+                </div>
+        </div>
+    <div class="service_details">
+      </div>
+    </div>`
+}
---
+++ services.php
@@ -0,0 +1,11 @@
+<?php
+    $sql = "SELECT * FROM services WHERE id=4;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if($resultCheck > 0){
+    while($row = $result->fetch_assoc()){
+    echo "<h3 class='service_title'>" . $row['title'] . "<br>" . "</h3>";
+    echo "<p class='service_description'>" . $row['description'] . "<br>" . "</p>";
+}}
+?>
