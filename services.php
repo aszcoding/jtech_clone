aa4d219efcf0fd00161382984b730364a744d0c8
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-03 17:46:15 -0600

    update php

--- services.php
+++ services.php
@@ -1,18 +1,12 @@
 <?php 
-include("main_services_query.php");
+include("title_services_query.php");
 
 
 
  $element = "<div class='service'>";
  $element2= "<div class='img_container'>";
  $element3= "<img class='service_img'/></div>";
- $element4= "<div class='overlay'></div>";
- 
-if($resultCheck > 0){
-while($row = $result->fetch_assoc()){
-echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
-}}
-
+ $element4= "<div class='overlay'>'title_services_query.php'</div>";
  $element5= "<div class='service_details'></div></div>";
  $count = 6;
  foreach( range(1,$count) as $item){
