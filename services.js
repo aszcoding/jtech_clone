bf73c4416aecfd88539745cc87f05b1a76d3b780
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-03 08:31:10 -0600

    update services.js

--- services.js
+++ services.js
@@ -1,6 +1,12 @@
 for(var i=0; i<6; i++){
 document.getElementById("services").innerHTML +=
 `<div class = "service">
+<?php
+if($resultCheck > 0){
+    while($row = $result->fetch_assoc()){
+      echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
+    }}
+?>
     <div class = "img_container">
         <img class = "service_img"/>
     </div>
