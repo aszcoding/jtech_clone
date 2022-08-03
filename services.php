decc13b85ef198cc01aa961c971f6ec48d0fc516
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-03 17:35:24 -0600

    update php

--- services.php
+++ services.php
@@ -6,9 +6,7 @@ include("main_services_query.php");
  $element = "<div class='service'>";
  $element2= "<div class='img_container'>";
  $element3= "<img class='service_img'/></div>";
- $element4= "<div class='overlay'>
- 
- </div>";
+ $element4= "<div class='overlay'></div>";
  $element5= "<div class='service_details'></div></div>";
  $count = 6;
  foreach( range(1,$count) as $item){
