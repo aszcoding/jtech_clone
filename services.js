488656983d84e1dd14449ad6258714bddeb411b9
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-03 11:13:42 -0600

    Updated js and php files

--- services.js
+++ services.js
@@ -7,12 +7,12 @@ document.getElementById("services").innerHTML +=
         <img class = "service_img"/>
     </div>
 <div class = "overlay">
-`<?php
+<?php
 if($resultCheck > 0){
     while($row = $result->fetch_assoc()){
       echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
     }}
-?>`
+?>
 </div>
 <div class = "service_details>
 <?php
--- services.php
+++ services.php
@@ -1,7 +1,11 @@
 <?php 
- $element = "<div></div>";
+ $element = "<div class='service'></div>";
+ $element2= "<div class='img_container'></div>";
+ $element3= "<img class='service_img'></div>";
+ $element4= "<div class='overlay'></div>";
+ $element5= "<div class='service_details'></div>";
  $count = 6;
  foreach( range(1,$count) as $item){
-     echo $element;
+     echo $element, $element2, $element3, $element4, $element5;
  }
 ?>
