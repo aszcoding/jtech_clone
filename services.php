d21cc2ee739df45e19207606af0d857e41be8913
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-03 16:50:24 -0600

    update php

--- services.php
+++ services.php
@@ -4,11 +4,32 @@ include("main_services_query.php");
  $element = "<div class='service'>";
  $element2= "<div class='img_container'>";
  $element3= "<img class='service_img'/></div>";
- $element4= "<div class='overlay'>".include("title_services_query.php");"</div>";
+ $element4= "<div class='overlay'></div>";
  $element5= "<div class='service_details'></div></div>";
  $count = 6;
  foreach( range(1,$count) as $item){
      echo $element, $element2, $element3, $element4, $element5;
- }
+
+
+foreach( range(1, $element4) as $title){
+    if($resultCheck > 0){
+        while($row = $result->fetch_assoc()){
+            echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
+        }}
+ }}
 
  ?>
+
+
+<!-- if($resultCheck > 0){
+while($row = $result->fetch_assoc()){
+    echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
+    echo "<p class='service_description'>" . $row['description'] . "<br>" . "</p>";
+}}
+           
+if($resultCheck > 0){
+while($row = $result->fetch_assoc()){
+echo "<h3 class='service_title'>" . $row['service_title'] . "<br>" . "</h3>";
+}}
+
+?> -->
