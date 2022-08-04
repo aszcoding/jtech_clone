f8105b0155afe8a6c68cdd7938299c562196d2d2
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-04 07:55:34 -0600

    Update php

--- services.php
+++ services.php
@@ -2,7 +2,16 @@
     while ($row = $result->fetch_assoc()) { ?>
         <div class="service">
             <div class="img_container">
-                <img class="service_img"<?php echo $row["img_src"]; ?> />
+            <?php 
+            $name = 'img_name';
+            $stmt = $pdo->prepare("SELECT `img_data` FROM `images` WHERE `img_name`=?");
+            $stmt->execute([$name]);
+            $img = $stmt->fetch();
+            $img = $img["img_src"];
+            $img = base64_encode($img);
+            $ext = pathinfo($name, PATHINFO_EXTENSION);
+            echo "<img src='data:image/".$ext.";base64,".$img."'/>";
+            ?>
             </div>
             <div class="overlay">
                 <h3 class="service_title"><?php echo $row["service_title"]; ?></h3>
