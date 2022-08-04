c575874adaff0df67175baa38af1ba0659b3a555
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-04 07:15:13 -0600

    updated php file

--- services.php
+++ services.php
@@ -1,14 +1,14 @@
-<div class="container">
-    <?php if ($resultCheck > 0) {
-        while ($row = $result->fetch_assoc()) { ?>
-            <div class="service">
-                <h3 class="service_title"><?php echo $row["service_title"]; ?></h3>
-                <div class="img_container">
-                    <img class="service_img" />
-                </div>
-                <div class="overlay"></div>
-                <div class="service_details"></div>
+<?php if ($resultCheck > 0) {
+    while ($row = $result->fetch_assoc()) { ?>
+        <div class="service">
+            <div class="img_container">
+                <img class="service_img" />
             </div>
+            <div class="overlay">
+                <h3 class="service_title"><?php echo $row["service_title"]; ?></h3>
+            </div>
+            <div class="service_details"></div>
+        </div>
     <?php }
     } ?>
 </div>
