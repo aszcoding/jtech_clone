d93db348eed944ec5a91648622178d829652492f
Ashley Serrano-Ziel <103522919+smashley127@users.noreply.github.com>
2022-08-05 11:48:28 -0600

    update php

--- services.php
+++ services.php
@@ -2,6 +2,7 @@
     while ($row = $result->fetch_assoc()) { ?>
         <div class="service">
             <div class="img_container">
+                <h3 class="service_title"><?php echo $row["service_title"]; ?></h3>
                 <?php if($row["img_src"] != null) {
                     echo '<img src="data:image/jpg;base64,'. base64_encode($row["img_src"]).' "class="service_img" "alt='.$row['img_alt_txt'].'" />';
                 }else{
@@ -9,9 +10,9 @@
                 }
                 ?>
             </div>
-            <div class="overlay">
-                <h3 class="service_title"><?php echo $row["service_title"]; ?></h3>
-            </div>
+            <!-- <div class="overlay">
+
+            </div> -->
             <div class="service_details">
             <p class="service_description"><?php echo $row["description"]; ?></p>
             </div>
