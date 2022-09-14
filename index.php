fe1a3e9f89a404ac1029f0f05722f6968f569fa3
Ashley Serrano-Ziel <92651735+aszcoding@users.noreply.github.com>
2022-09-14 11:33:36 -0600

    Update img file path

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">  
   <title>Services</title>
   <link rel="icon" type="favicon" href="assets/img/favicon/club_wellness_favicon.png" alt="Club Wellness favicon">
-<link rel="preload" href='style.css' as="style" onload="this.rel = 'stylesheet'">
+<link rel="stylesheet" type="text/css" href='style.css'>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
