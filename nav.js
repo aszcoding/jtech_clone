551e563b18fd1c199f76eac5f15b2729334b343d
Ashley Serrano-Ziel <92651735+aszcoding@users.noreply.github.com>
2022-09-16 16:32:02 -0600

    update nav.js

--- nav.js
+++ nav.js
@@ -20,7 +20,7 @@ function createTopNav(){
     </div>
     <nav class="full_nav">
         <ul class="top_links">
-            <li class="top set1"><a onclick='classList.add('active')' href="index.php">Home</a></li>
+            <li class="top set1"><a class="active" href="index.php">Home</a></li>
             <li class="top set1"><a href="services.php">Services</a></li>
             <li class="logo_container"><img class="top_logo" src="assets/img/main/club-wellness-evolutions.svg" alt="Club Wellness Evolutions"></li>
             <li class="top set2"><a href="about.php">About</a></li>
@@ -90,3 +90,4 @@ function createFooter(){
 };
 
 createFooter();
+
