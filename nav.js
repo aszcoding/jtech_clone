0a023870ccb9f30b1e5868c76ea6992617742495
Ashley Serrano-Ziel <92651735+aszcoding@users.noreply.github.com>
2022-09-16 07:35:27 -0600

    Update js

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 const fullNav = () => {
-    document.getElementsByTagName("header").insertAdjacentHTML('afterend', 
+    document.getElementsByTagName("header").innerHTML +=
     `<nav id="full_nav">
         <ul class="top_links">
             <li class="top set1"><a class="active" href="index.php">Home</a></li>
@@ -9,4 +9,4 @@ const fullNav = () => {
             <li class="top set2"><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
-`)};
+`};
